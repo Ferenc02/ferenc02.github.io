0b51d28c966362ad8fe4fc49f2f6a0922706886a
ferenc02 <ferenc.sziraki@medieinstitutet.se>
2024-12-13 18:23:17 +0100

    Set up Tailwind CSS and Svelte configuration with initial styles and components

---
+++ src/routes/components/_footer.svelte
@@ -0,0 +1,9 @@
+<script>
+	import ThemeSwitcher from '../components/_theme_switcher.svelte';
+</script>
+
+<footer class="flex w-full justify-between self-end p-4 align-middle">
+	<div></div>
+	<p class="text-center">&copy; 2024 Ferenc Sziraki</p>
+	<ThemeSwitcher />
+</footer>
---
+++ src/routes/components/_header.svelte
@@ -0,0 +1,13 @@
+<header class="min-w-full p-4 md:p-8">
+	<nav class="flex justify-around">
+		<div class="logo">Logo</div>
+		<ul class="flex gap-4">
+			<li class="mr-2 hidden hover:underline md:block">
+				<a href="/">Home</a>
+			</li>
+			<li class="mr-2 hover:underline"><a href="/projects">Projects</a></li>
+			<li class="mr-2 hover:underline"><a href="/about">About</a></li>
+			<li class="mr-2 hover:underline"><a href="/contact">Contact </a></li>
+		</ul>
+	</nav>
+</header>
