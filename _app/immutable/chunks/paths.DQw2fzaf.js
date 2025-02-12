b75abbcbbdb3183ecb6346ae12f65efe2171c666
Ferenc02 <ferenc.sziraki@medieinstitutet.se>
2025-02-12 21:10:57 +0100

    Updates

---
+++ _app/immutable/chunks/paths.DQw2fzaf.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_rp0ift)==null?void 0:s.base)??"/ferenc02.github.io";var t;const a=((t=globalThis.__sveltekit_rp0ift)==null?void 0:t.assets)??e;export{a,e as b};
