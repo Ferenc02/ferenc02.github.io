e5d682906ac1d68242a4b81c074acf198d74719d
Ferenc02 <ferenc.sziraki@medieinstitutet.se>
2025-02-12 21:06:12 +0100

    Add more stuff

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -33,26 +33,83 @@
 	description="This collection showcases projects coded by Ferenc. Projects ranging from fractals, website design, and more."
 />
 
-<section class="flex h-full w-full px-6 py-12 text-center align-middle" in:fade={{ duration: 300 }}>
-	<div class="container mx-auto max-w-4xl">
-		<h1 class="py-4 text-4xl font-bold">Nature of Code</h1>
-		<p class="dark:text-gray-40 text-lg text-gray-600 dark:text-gray-400">
-			This collection showcases projects inspired by <b>The Nature of Code</b> by Daniel Shiffman, a
-			foundational resource that bridges the gap between programming and the natural world. Through these
-			projects, I explore how algorithms can replicate patterns and phenomena seen in nature, from fractals
-			to emergent behavior, revealing the profound connection between code and the natural world.
-		</p>
-		<!--{base}/projects/Nature-Of-Code/{folder}/index.html //If you want to go to the static link-->
-		<!--{base}/projects/{folder} //If you want sveltekit own rendering-->
-		<div class="mt-8 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-			{#each folders as folder}
+<div class="flex h-full w-full flex-col items-center justify-center">
+	<section
+		class="flex h-full w-full px-6 py-12 text-center align-middle"
+		in:fade={{ duration: 300 }}
+	>
+		<div class="container mx-auto max-w-4xl">
+			<h1 class="py-4 text-4xl font-bold">Projects</h1>
+			<p class="dark:text-gray-40 text-lg text-gray-600 dark:text-gray-400">
+				Here are some other projects I've worked on and contributed to. These projects range from
+				web development to blockchain and more. Feel free to check them out on GitHub.
+			</p>
+
+			<div class="mt-8 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
 				<a
-					href="{base}/projects/{folder}"
+					href="https://github.com/Ferenc02/Westcoast-Education"
+					target="_blank"
 					class=" flex transform-gpu items-center justify-center rounded-lg bg-white p-6 shadow-lg transition-transform hover:scale-105 dark:bg-neutral-700"
 				>
-					<h2 class="text-xl font-bold">{folder}</h2>
+					<h2 class="text-xl font-bold">Westcoast-Education</h2>
 				</a>
-			{/each}
+				<a
+					href="https://github.com/Ferenc02/Block-Explorer"
+					target="_blank"
+					class=" flex transform-gpu items-center justify-center rounded-lg bg-white p-6 shadow-lg transition-transform hover:scale-105 dark:bg-neutral-700"
+				>
+					<h2 class="text-xl font-bold">Block-Explorer</h2>
+				</a>
+				<a
+					href="https://github.com/Ferenc02/WasmFusion-PDF-Backend"
+					target="_blank"
+					class=" flex transform-gpu items-center justify-center rounded-lg bg-white p-6 shadow-lg transition-transform hover:scale-105 dark:bg-neutral-700"
+				>
+					<h2 class="text-xl font-bold">WasmFusion-PDF (work in progress)</h2>
+				</a>
+
+				<a
+					href="https://github.com/Ferenc02/bcu24d-agila-block-the-chain-group3"
+					target="_blank"
+					class=" flex transform-gpu items-center justify-center rounded-lg bg-white p-6 shadow-lg transition-transform hover:scale-105 dark:bg-neutral-700"
+				>
+					<h2 class="text-xl font-bold">Blockchain for dummies 101</h2>
+				</a>
+				<a
+					href="https://github.com/K3mp3/JS-gruppuppgift"
+					target="_blank"
+					class=" flex transform-gpu items-center justify-center rounded-lg bg-white p-6 shadow-lg transition-transform hover:scale-105 dark:bg-neutral-700"
+				>
+					<h2 class="text-xl font-bold">Travel Plan</h2>
+				</a>
+			</div>
 		</div>
-	</div>
-</section>
+	</section>
+	<section
+		class="flex h-full w-full px-6 py-12 text-center align-middle"
+		in:fade={{ duration: 300 }}
+	>
+		<div class="container mx-auto max-w-4xl">
+			<h1 class="py-4 text-4xl font-bold">Nature of Code</h1>
+			<p class="dark:text-gray-40 text-lg text-gray-600 dark:text-gray-400">
+				This collection showcases projects inspired by <b>The Nature of Code</b> by Daniel Shiffman,
+				a foundational resource that bridges the gap between programming and the natural world. Through
+				these projects, I explore how algorithms can replicate patterns and phenomena seen in nature,
+				from fractals to emergent behavior, revealing the profound connection between code and the natural
+				world.
+			</p>
+			<!--{base}/projects/Nature-Of-Code/{folder}/index.html //If you want to go to the static link-->
+			<!--{base}/projects/{folder} //If you want sveltekit own rendering-->
+			<div class="mt-8 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+				{#each folders as folder}
+					<a
+						href="{base}/projects/{folder}"
+						class=" flex transform-gpu items-center justify-center rounded-lg bg-white p-6 shadow-lg transition-transform hover:scale-105 dark:bg-neutral-700"
+					>
+						<h2 class="text-xl font-bold">{folder}</h2>
+					</a>
+				{/each}
+			</div>
+		</div>
+	</section>
+</div>
