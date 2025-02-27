54ee21a69492c0db341a96fa709c2b99dfb0d34d
Ferenc02 <ferenc.sziraki@medieinstitutet.se>
2025-02-27 17:51:26 +0100

    Updates

--- _app/immutable/chunks/paths.CUYyOscI.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1jm5ttn)==null?void 0:s.base)??"/ferenc02.github.io";var t;const a=((t=globalThis.__sveltekit_1jm5ttn)==null?void 0:t.assets)??e;export{a,e as b};
