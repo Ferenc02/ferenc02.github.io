cf258597523402cffcb4bf5a6ac8fd658e48e6f7
Ferenc02 <ferenc.sziraki@medieinstitutet.se>
2025-03-28 14:01:22 +0100

    Refactor project sections for improved layout and readability

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -36,12 +36,12 @@
 
 <div class="flex h-full w-full flex-col items-center justify-center">
 	<section
-		class="flex h-full w-full px-6 py-12 text-center align-middle"
+		class="flex h-full w-full px-6 py-4 text-center align-middle"
 		in:fade={{ duration: 300 }}
 	>
 		<div class="container mx-auto max-w-4xl">
-			<h1 class="pb-4 text-4xl font-bold">Many of my projects are open-source</h1>
-			<p class="dark:text-gray-40 text-lg text-gray-600 dark:text-gray-400">
+			<h1 class=" my-4 text-4xl font-bold">Many of my projects are open-source</h1>
+			<p class="dark:text-gray-40 text-left text-lg text-gray-600 dark:text-gray-400">
 				Here are some of the open-source projects I've worked on and contributed to. These projects
 				range from web development to blockchain and more. Feel free to check them out on GitHub.
 			</p>
@@ -50,17 +50,24 @@
 					href="https://github.com/Ferenc02"
 					target="_blank"
 					class="
-					flex items-center justify-center
-					
-					rounded-lg bg-white px-6 py-3 font-bold text-black shadow-md transition-transform hover:scale-105 dark:bg-neutral-700 dark:text-white"
+				flex items-center justify-center
+				
+				rounded-lg bg-white px-6 py-3 font-bold text-black shadow-md transition-transform hover:scale-105 dark:bg-neutral-700 dark:text-white"
 				>
 					<img src={githubSVG} alt="GitHub" class="mr-2 inline-block h-6 w-6 filter dark:invert" />
 					<span>GitHub</span>
 				</a>
 			</div>
+		</div>
+	</section>
 
-			<h1 class="mt-8 py-4 text-4xl font-bold">Projects</h1>
-			<p class="dark:text-gray-40 text-lg text-gray-600 dark:text-gray-400">
+	<section
+		class="flex h-full w-full px-6 py-4 text-center align-middle"
+		in:fade={{ duration: 300 }}
+	>
+		<div class="container mx-auto max-w-4xl">
+			<h1 class=" my-4 text-4xl font-bold">Projects</h1>
+			<p class="dark:text-gray-40 text-left text-lg text-gray-600 dark:text-gray-400">
 				Here are some larger projects I've worked on. These projects range from web development to
 				blockchain and more.
 			</p>
@@ -127,12 +134,12 @@
 		</div>
 	</section>
 	<section
-		class="flex h-full w-full px-6 py-12 text-center align-middle"
+		class="flex h-full w-full px-6 py-4 text-center align-middle"
 		in:fade={{ duration: 300 }}
 	>
 		<div class="container mx-auto max-w-4xl">
-			<h1 class="py-4 text-4xl font-bold">Archives</h1>
-			<p class="dark:text-gray-40 text-lg text-gray-600 dark:text-gray-400">
+			<h1 class="my-4 text-4xl font-bold">Archives</h1>
+			<p class="dark:text-gray-40 text-left text-lg text-gray-600 dark:text-gray-400">
 				Here are some of the projects I've worked on in the past. These projects contains a variety
 				of web development projects, from websites to web applications, also some Java projects.
 				Most of these projects were made during my time at <b>Lindholmens Tekniska Gymnasium</b> and
@@ -195,12 +202,12 @@
 		</div>
 	</section>
 	<section
-		class="flex h-full w-full px-6 py-12 text-center align-middle"
+		class="flex h-full w-full px-6 py-4 text-center align-middle"
 		in:fade={{ duration: 300 }}
 	>
 		<div class="container mx-auto max-w-4xl">
-			<h1 class="py-4 text-4xl font-bold">Nature of Code</h1>
-			<p class="dark:text-gray-40 text-lg text-gray-600 dark:text-gray-400">
+			<h1 class=" my-4 text-4xl font-bold">Nature of Code</h1>
+			<p class="dark:text-gray-40 text-left text-lg text-gray-600 dark:text-gray-400">
 				This collection showcases projects inspired by <b>The Nature of Code</b> by Daniel Shiffman,
 				a foundational resource that bridges the gap between programming and the natural world. Through
 				these projects, I explore how algorithms can replicate patterns and phenomena seen in nature,
